a01ed87d3a8e179a749345ec6da55d57dd1899b3
Ubuntu <ubuntu@ip-172-31-23-203.ap-southeast-2.compute.internal>
2013-08-26 16:30:59 +0000

    PartB input and result functionalities done. Part of a name done. Still have to input validation and a bit of formatting.

--- partB/results_screen.php
+++ partB/results_screen.php
@@ -10,7 +10,8 @@
 <body bgcolor="white">
 <?php
 
-  function showerror() {
+  function showerror() 
+  {
      die("Error " . mysql_errno() . " : " . mysql_error());
   }
 
@@ -18,16 +19,19 @@
 
   function displayWinesList($connection, $query) {
     
-    if (!($result = @ mysql_query ($query, $connection))) {
+    if (!($result = @ mysql_query ($query, $connection))) 
+    {
       showerror();
     }
 
     $rowsFound = @ mysql_num_rows($result);
 
-    if ($rowsFound > 0) {
-      print "Details of your wine search<br>";
-
-      print "\n<table>\n<tr>" .
+    if ($rowsFound > 0) 
+    {
+      print "\n<table border='1'>" .
+          "\n\t<caption>Details of your wine search</caption>\n" .
+          "\n<thead>" .
+          "\n\t<tr>" .
           "\n\t<th>Wine name</th>" .
           "\n\t<th>Wine variety</th>" .
           "\n\t<th>Year</th>" .
@@ -36,10 +40,10 @@
           "\n\t<th>Cost in the inventory</th>" .
           "\n\t<th>Quantity on hand</th>" .
           "\n\t<th>Quantity sold</th>" . 
-          "\n\t<th>Total sales</th>\n</tr>"; 
+          "\n\t<th>Total sales</th>\n</tr></thead>"; 
 
-      while ($row = @ mysql_fetch_array($result)) {
-        
+      while ($row = @ mysql_fetch_array($result)) 
+      {  
         print "\n<tr>\n\t<td>{$row["wine_name"]}</td>" .
             "\n\t<td>{$row["variety"]}</td>" .
             "\n\t<td>{$row["year"]}</td>" .
@@ -49,13 +53,19 @@
             "\n\t<td>{$row["on_hand"]}</td>" .
             "\n\t<td>{$row["qty"]}</td>".
             "\n\t<td>{$row["SUM(items.price)"]}</td>\n</tr>";
-
       }
 
       print "\n</table>";
     } 
     
-    print "{$rowsFound} records found matching your criteria<br>";
+    if ($rowsFound > 0)
+    {
+      print "{$rowsFound} records found matching your criteria<br>";
+    }
+    else
+    {
+      print "No records match your search criteria<br>";
+    }
   } 
   
   if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW))) {
@@ -65,6 +75,14 @@
   // get the user data
   $wineName = $_GET['wineName'];
   $wineryName = $_GET['wineryName'];
+  $regionName = $_GET['regionName']; 
+  $grapeVariety = $_GET['grapeVariety'];
+  $wineYearLowBound = $_GET['wineYearLowBound'];
+  $wineYearUpBound = $_GET['wineYearUpBound'];
+  $wineMinStock = $_GET['wineMinStock'];
+  $wineMinOrder = $_GET['wineMinOrder'];
+  $wineMinCost = $_GET['wineMinCost'];
+  $wineMaxCost = $_GET['wineMaxCost'];
 
   if (!mysql_select_db(DB_NAME, $connection)) {
     showerror();
@@ -80,13 +98,50 @@ AND wine.winery_id = winery.winery_id
 AND wine.wine_id = inventory.wine_id
 AND wine.wine_id = items.wine_id";
 
-  if (isset($wineName) && $wineName != "") {
-    $query .= " AND wine_name = '{$wineName}'";
+  if (isset($wineName) && $wineName != "") 
+  {
+    $query .= " AND wine_name LIKE '%{$wineName}%'";
   }
 
-  if (isset($wineryName) && $wineryName != "") {
-    $query .= " AND winery_name = '{$wineryName}'";
+  if (isset($wineryName) && $wineryName != "") 
+  {
+    $query .= " AND winery_name LIKE '%{$wineryName}%'";
   }
+  if (isset($regionName) && $regionName != "All")
+  {
+    $query .= " AND region_name = '{$regionName}'";
+  }
+  if (isset($grapeVariety) && $grapeVariety != "All")
+  {
+    $query .= " AND variety = '{$grapeVariety}'";
+  }
+  if (isset($wineYearLowBound) && $wineYearLowBound != "----")
+  {
+    $query .= " AND year >= '{$wineYearLowBound}'";
+  }
+  if (isset($wineYearUpBound) && $wineYearUpBound != "----")
+  {
+    $query .= " AND year <= '{$wineYearUpBound}'";
+  }
+  if (isset($wineMinStock) && $wineMinStock != "")
+  {
+    $query .= " AND on_hand  >= '{$wineMinStock}'";
+  }
+  if (isset($wineMinOrder) && $wineMinOrder != "")
+  {
+    $query .= " AND qty  >= '{$wineMinOrder}'";
+  }
+  if (isset($wineMinCost) && $wineMinCost != "")
+  {
+    $query .= " AND cost  >= '{$wineMinCost}'";
+  }
+  if (isset($wineMaxCost) && $wineMaxCost != "")
+  {
+    $query .= " AND cost  <= '{$wineMaxCost}'";
+  }
+
+
+
 
   $query .= " GROUP BY items.wine_id
   ORDER BY wine_name, year";
--- partB/search_screen.php
+++ partB/search_screen.php
@@ -6,13 +6,77 @@
   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
   <title>Wine is wine</title>
 </head>
+<body>
   <form action="results_screen.php" method="GET">
     <br>A wine name (or part of a wine name)
     <input type="text" name="wineName" value="">
     <br>A winery name (or part of a winery name)
     <input type="text" name="wineryName" value="">
-    <br><input type="submit" value="Show wines">
+
+    <br> <?php
+        require "db.php";
+
+        function selectDistinct ($connection, $tableName, $attributeName, $pulldownName, $defaultValue) 
+        {
+            $defaultWithinResultSet = FALSE;
+
+            $distinctQuery = "SELECT DISTINCT {$attributeName} FROM
+            {$tableName} ORDER BY {$attributeName}";
+
+            if (!($resultId = @ mysql_query ($distinctQuery, $connection)))
+                showerror();
+
+            print "\n<select name=\"{$pulldownName}\">";
+
+            while ($row = @ mysql_fetch_array($resultId))
+            {
+                $result = $row[$attributeName];
+
+                if (isset($defaultvalue) && $result == $defaultValue)
+                    print "\n\t<option selected value=\"{$result}\">{$result}";
+                else
+                    print "\n\t<option value=\"{$result}\">{$result}";
+                print "</option>";
+            }
+            print "\n</select>";
+        }  
+
+        if (!($connection = @ mysql_connect(DB_HOST, DB_USER, DB_PW)))
+        {
+            showerror();
+        }
+
+        if (!mysql_select_db(DB_NAME, $connection)) 
+        {
+            showerror();
+        }
+
+        print "<br>\nRegion: ";
+        selectDistinct($connection, "region", "region_name", "regionName", "All");
+        print "<br>\nGrape Variety: ";
+        selectDistinct($connection, "grape_variety", "variety", "grapeVariety", "All");
+        print "<br>\nFrom: ";
+        selectDistinct($connection, "wine", "year", "wineYearLowBound", "----");
+        print "\nto: ";
+        selectDistinct($connection, "wine", "year", "wineYearUpBound", "----");
+
+    ?>
+    <br>Minimum wine quantity in stock:
+    <input type="text" name="wineMinStock" value="">
+    <br>Minimum wine quantity ordered:
+    <input type="text" name="wineMinOrder" value="">
+    <br>Wine minimum cost:
+    <input type="text" name="wineMinCost" value="">
+    <br>Wine maximum cost:
+    <input type="text" name="wineMaxCost" value="">
+ 
+    <br><input type="submit" name="submit" value="Show Wines">
   </form>
 </body>
 </html>
+
+
+
+
+
 
