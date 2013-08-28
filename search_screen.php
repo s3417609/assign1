3d29cc72c2fbeab1795f1458d1038678f3966e4a
Ubuntu <ubuntu@ip-172-31-23-203.ap-southeast-2.compute.internal>
2013-08-28 15:35:03 +0000

    Input Validation done, new css for it too

--- search_screen.php
+++ search_screen.php
@@ -36,6 +36,104 @@ if (!mysql_select_db(DB_NAME, $connection))
   showerror();
 }
 
+$wineName = $wineryName = $wineMinStock = $wineMinOrder = $wineMinCost = $wineMaxCost = '';
+
+$err_year = $err_stock = $err_order = $err_min_cost = $err_max_cost = '';
+
+if (isset($_GET['submit']))
+{
+  $wineName=trim($_GET['wineName']);
+  $wineryName= trim($_GET['wineryName']);
+  $regionName=$_GET['regionName'];
+  $grapeVariety=$_GET['grapeVariety'];
+  $wineYearLowBound=$_GET['wineYearLowBound'];
+  $wineYearUpBound=$_GET['wineYearUpBound'];
+  $wineMinStock=trim($_GET['wineMinStock']);
+  $wineMinOrder=trim($_GET['wineMinOrder']);
+  $wineMinCost=trim($_GET['wineMinCost']);
+  $wineMaxCost=trim($_GET['wineMaxCost']);
+
+  // Year input validation
+  if($_GET['wineYearUpBound'] < $_GET['wineYearLowBound'])
+  {
+    $err_year="*'From' year must be inferior to 'To' year";
+  }
+
+  // Stock input validation
+  if(!empty($wineMinStock))
+  {
+    if(is_numeric($wineMinStock))
+    {
+      if((int)$wineMinStock < 0)
+      {
+        $err_stock = "*A quantity cannot be negative";
+      }
+    }
+    else
+      $err_stock = "*Please input a numeric value.";
+  }
+
+  // Order input validation
+  if(!empty($wineMinOrder))
+  {
+    if(is_numeric($wineMinOrder))
+    {
+      if((int)$wineMinOrder < 0)
+      {
+        $err_order = "*A quantity cannot be negative";
+      }
+    }
+    else
+      $err_order = "*Please input a numeric value.";
+  }
+
+  // Min Cost input validation
+  if(!empty($wineMinCost))
+  {
+    if(is_numeric($wineMinCost))
+    {
+      if((double)$wineMinCost < 0)
+      {
+        $err_min_cost = "*A cost cannot be negative";
+      }
+    }
+    else
+      $err_min_cost = "*Please input a numeric value.";
+  }
+
+  // Max Cost input validation
+  if(!empty($wineMaxCost))
+  {
+    if(is_numeric($wineMaxCost))
+    {
+      if((double)$wineMaxCost < 0)
+      {
+        $err_max_cost = "*A cost cannot be negative";
+      }
+    }
+    else
+      $err_max_cost = "*Please input a numeric value.";
+  }
+
+  // Cost range input validation
+  if(!empty($wineMinCost) && !empty($wineMaxCost))
+  {
+    if(is_numeric($wineMinCost) && is_numeric($wineMaxCost))
+    {         
+      if((double)$wineMinCost>(double)$wineMaxCost)
+      {
+        $err_min_cost="*Maximum cost must be superior to minimum cost.";
+        $err_max_cost="*Maximum cost must be superior to minimum cost.";
+      }
+    }
+  }
+  
+  if(($err_year == "") && ($err_stock == "") && ($err_order == "") && ($err_min_cost == "") && ($err_max_cost == ""))
+  {
+    header("Location: results_screen.php?wineName=".$wineName."&wineryName=".$wineryName."&regionName=".$regionName."&grapeVariety=".$grapeVariety."&wineMinStock=".$wineMinStock."&wineMinOrder=".$wineMinOrder."&wineMinCost=".$wineMinCost."&wineMaxCost=".$wineMaxCost."&wineYearLowBound=".$wineYearLowBound."&wineYearUpBound=".$wineYearUpBound);
+  }
+  
+}
 ?>
 
 
@@ -45,11 +143,12 @@ if (!mysql_select_db(DB_NAME, $connection))
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
 <title>Wine is wine</title>
 </head>
 <body>
 
-  <form action="results_screen.php" method="GET">
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="GET">
 <br>A wine name (or part of a wine name)
   <input type="text" name="wineName" value="">
 <br>A winery name (or part of a winery name)
@@ -64,16 +163,24 @@ if (!mysql_select_db(DB_NAME, $connection))
   selectDistinct($connection, "wine", "year", "wineYearLowBound", "");
   print "\nto: ";
   selectDistinct($connection, "wine", "year", "wineYearUpBound", "");
+  echo "<span class='error'>".$err_year."</span>";
 
   ?>
   <br>Minimum wine quantity in stock:
   <input type="text" name="wineMinStock" value="">
+  <span class="error"><?php echo $err_stock;?></span>
+
   <br>Minimum wine quantity ordered:
   <input type="text" name="wineMinOrder" value="">
+  <span class="error"><?php echo $err_order;?></span>
+
   <br>Wine minimum cost:
   <input type="text" name="wineMinCost" value="">
+  <span class="error"><?php echo $err_min_cost;?></span>
+  
   <br>Wine maximum cost:
   <input type="text" name="wineMaxCost" value="">
+  <span class="error"><?php echo $err_max_cost;?></span>
  
  <br><input type="submit" name="submit" value="Show Wines">
   </form>
